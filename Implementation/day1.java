0d30f4f7738d619e25ea0f5e09d73165d0befe40
kshitij0209ag <kshitij.agrawal0209@gmail.com>
2022-02-14 21:44:57 +0530

    code

--- Implementation/day1.java
+++ Implementation/day1.java
@@ -6,7 +6,7 @@ import javax.sound.sampled.SourceDataLine;
 
 public class day1 {
     public static void main(String args[]) {
-        int n;
+        int n,m,p;
 
         // input the no. from user
 
@@ -15,14 +15,34 @@ public class day1 {
         n = sc.nextInt();
 
         int a[] = new int[n];
+        int b[] = new int[n+1];
+
         for(int i = 0; i<n; i++){
             a[i] = sc.nextInt();
         }
 
+        System.out.print("Enter Index of new Value to be Inserted");
+        m = sc.nextInt();
+        System.out.print("Enter new Value to be Inserted");
+        p = sc.nextInt();
+        for(int i = 0; i < n+1; i++){
+            if(i<m){
+                b[i] = a[i];
+            }
+            else if(i==m){
+                b[i] = p;
+            }
+            else{
+                b[i] = a[i-1];
+            }
+        }
+
         // Implemetation of how to create array 
         System.out.print("Elements are: ");
-        for(int i = 0; i < n; i++){
-            System.out.println(a[i]);
+        for(int i = 0; i < n+1; i++){
+            // creation array
+            // System.out.println(a[i]);
+            System.out.println(b[i]);
         }
     }
 

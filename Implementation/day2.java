f0dabf283119dd3dc8d2faf219bc1806fb893f33
kshitij0209ag <kshitij.agrawal0209@gmail.com>
2022-02-15 15:28:47 +0530

    Multidimesional Arrays

--- Implementation/day2.java
+++ Implementation/day2.java
@@ -11,9 +11,19 @@ public class day2 {
         m = sc.nextInt();
         int a[][] = new int[n][m];
         System.out.print("Enter Values:- ");
-        for(int i = 0; i <n; i++){                  // n=2, m=3
-            for(int j = 0; j <m; j++){
+        for(int i = 0; i <n; i++)
+        {                  // n=2, m=3
+            for(int j = 0; j <m; j++)
+            {
                 a[i][j] = sc.nextInt();         // a[0][0], a[0][1], a[0][2] / a[1][0], a[1][1], a[1][2]
+            }
+        }
+
+        for (int i = 0; i < n; i++) 
+        { // n=2, m=3
+            for (int j = 0; j < m; j++) 
+            {
+                a[i][j] = sc.nextInt(); // a[0][0], a[0][1], a[0][2] / a[1][0], a[1][1], a[1][2]
             }
         }
 

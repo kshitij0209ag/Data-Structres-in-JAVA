3dbed3896f8014f120d7bd68d7ae903253eabf7c
kshitij0209ag <kshitij.agrawal0209@gmail.com>
2022-02-15 15:07:18 +0530

    Multidimensional Array

--- Implementation/day2.java
+++ Implementation/day2.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class day2 {
-    
+    public static void main(String args[]){
+
+        int n, m, p;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no. of ROWS:- ");
+        n = sc.nextInt();
+        System.out.print("Enter no. of COLUMNS:- ");
+        m = sc.nextInt();
+        int a[][] = new int[n][m];
+        System.out.print("Enter Values:- ");
+        for()
+
+    }
 }

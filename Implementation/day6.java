2ef508cccc5161aceb7d62d7bab7565a8755bf62
kshitij0209ag <kshitij.agrawal0209@gmail.com>
2022-02-22 13:24:21 +0530

    Doubly linked list (creation + travere)

--- Implementation/day6.java
+++ Implementation/day6.java
@@ -1,3 +1,60 @@
+// Doubly linked list (creation + travere)
+
+import java.util.Scanner;
+
 public class day6 {
-    
+
+    // displaying how to create structure of Node , we make a class for Node
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head = null;
+
+    // now we make how to create a node
+    public void creation() {
+        int data, n;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("Enter Data");
+            data = sc.nextInt();
+
+            Node new_node = new Node(data);
+            if (head == null) {
+                head = new_node; // 30 20 10
+            } else {
+                new_node.next = head;
+                head = new_node;
+            }
+            System.out.print("Do u want to add more data, then press :1");
+            n = sc.nextInt();
+        } while (n == 1);
+    }
+
+    public void traverse() {
+        Node temp = head;
+        if (head == null) {
+            System.out.print("Linked list does not exist");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+
+        day6 ll = new day6();
+        ll.creation();
+        ll.traverse();
+
+    }
 }
